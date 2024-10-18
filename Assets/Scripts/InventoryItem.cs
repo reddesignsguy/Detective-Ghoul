8c8bb8c7dcfa7d3974ad9324b2c4c4000c0f75b4
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-10-18 00:05:48 -0700

    inventoery item scriptable object

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "NewInventoryItem", menuName = "InventoryItem")]
+public class InventoryItem : ScriptableObject
+{
+    public Texture2D image;
+    public string _name;
+    public string description;
+}
+
