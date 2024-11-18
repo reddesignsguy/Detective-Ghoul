62d2d19804c52933243f03a8c423a69f445ba732
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-11-17 21:02:09 -0800

    pictures page ui

---
+++ Assets/Scripts/UI/PicturesPageUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PicturesPageUI : UIManager
+{
+    public List<Image> imagePlaceholders;
+
+    public TextMeshProUGUI pageNumPlaceholder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Setup()
+    {
+
+    }
+}
