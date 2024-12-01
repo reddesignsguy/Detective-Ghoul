c057cbbf0ba775e9a1e6d8c2b8e02d681d1ec1b8
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-12-01 13:50:28 -0800

    Text dialogue fill out animation

--- Assets/Scripts/UI/DialogueUIManager.cs
+++ Assets/Scripts/UI/DialogueUIManager.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using DS.ScriptableObjects;
 using System;
+using System.Collections;
 
 public class DialogueUIManager : UIManager
 {
     private DSDialogueSO dialogue;
     public Text dialogueText;
+    public float fillOutRate = 14.0f;
+    private Coroutine fillOutcoroutine;
 
     [Obsolete]
     public void StartDialogueUI(Dialogue dialogue)
@@ -19,7 +22,22 @@ public class DialogueUIManager : UIManager
     public void SetUp(DSDialogueSO dialogue)
     {
         this.dialogue = dialogue;
-        dialogueText.text = dialogue.Text;
+        dialogueText.text = "";
+        StopFillingOut();
+        fillOutcoroutine = StartCoroutine(FillOutTextCoroutine());
+    }
+
+    private IEnumerator FillOutTextCoroutine()
+    {
+        IEnumerator it = dialogue.Text.GetEnumerator();
+        while (it.MoveNext())
+        {
+            dialogueText.text += it.Current;
+
+            // Yield until the next frame
+            yield return new WaitForSecondsRealtime(1f/fillOutRate);
+        }
+        fillOutcoroutine = null;
     }
 
     [Obsolete]
@@ -33,8 +51,21 @@ public class DialogueUIManager : UIManager
     {
         if (panel.activeSelf && Input.GetKeyDown(KeyCode.F))
         {
-            Skip();
+            if (!FillingOutFinished())
+            {
+                StopFillingOut();
+                dialogueText.text = dialogue.Text;
+            }
+            else
+            {
+                Skip();
+            }
         }
+    }
+
+    private bool FillingOutFinished()
+    {
+        return dialogueText.text == dialogue.Text;
     }
 
     private void Skip()
@@ -46,5 +77,12 @@ public class DialogueUIManager : UIManager
 
     public void FinishDialogue(){
          SetUIActive(false);  
+    }
+
+    private void StopFillingOut()
+    {
+        if (fillOutcoroutine != null)
+            StopCoroutine(fillOutcoroutine);
+        fillOutcoroutine = null;
     }
 }
