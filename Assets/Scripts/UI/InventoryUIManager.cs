01c3a2ea2f069e588b5009afadd2d506d159726b
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-12-03 16:44:56 -0800

    view item in inventory

--- Assets/Scripts/UI/InventoryUIManager.cs
+++ Assets/Scripts/UI/InventoryUIManager.cs
@@ -1,33 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
+public struct InventoryUIReferences
+{
+    public List<Image> itemImages;
+    public List<Image> placeholderImages;
+    public List<Button> buttons;
+
+    public int viewedItemIndex;
+    public GameObject viewedItemPlaceholder;
+}
 
 public class InventoryUIManager : UIManager
 {
 
     public InventorySystem inventory;
+    public GameObject toolbar;
+    public Image viewedItemImage;
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI description;
+
+    private InventoryUIReferences refs;
+
+    private void Awake()
+    {
+        refs = new InventoryUIReferences();
+        refs.itemImages = GetItemImages();
+        refs.placeholderImages = GetItemPlaceholders();
+        refs.buttons = GetButtons();
+        refs.viewedItemIndex = 0;
+        SetupButtons();
+    }
 
     private void OnEnable()
     {
-        Debug.Log(inventory.items.Count);
+        SetupToolbar();
+        SelectItem(refs.viewedItemIndex);
+    }
+
+
+    private void SetupToolbar()
+    {
         IEnumerator<ParsedInventoryItem> it = inventory.items.GetEnumerator();
 
-
-        Image[] images = transform.GetComponentsInChildren<Image>();
-        foreach (Image img in images)
+        // Set up tool bar
+        foreach (Image img in refs.itemImages)
         {
-            if (img.gameObject.name == "Holder" || img.gameObject == gameObject)
-                continue;
-
-            
+            // Fill slot with next item
             if (it.MoveNext())
             {
-                Debug.Log("Picture enabled");
-                img.enabled = true;
-
+                Debug.Log(it.Current.item);
                 ParsedInventoryItem item = it.Current;
-
+                img.enabled = true;
                 img.sprite = item.item.image;
             }
             else
@@ -38,17 +66,79 @@ public class InventoryUIManager : UIManager
         }
     }
 
-    private void OnDisable()
+    private void SelectItem(int index)
     {
-        Image[] images = transform.GetComponentsInChildren<Image>();
-        foreach (Image img in images)
+        if (index >= GetNumberOfItems())
         {
-            if (img.gameObject.name == "Holder" || img.gameObject == gameObject)
-                continue;
+            return;
+        }
 
+        GameObject placeholder = refs.placeholderImages[index].gameObject;
+        SetShadow(refs.viewedItemPlaceholder, false);
+        refs.viewedItemPlaceholder = placeholder;
+        refs.viewedItemIndex = index;
 
-            img.enabled = false;
+        // Update view
+        ViewItem(GetItem(index));
+        SetShadow(placeholder, true);
+    }
+
+    private void SetShadow(GameObject placeholder, bool set)
+    {
+        Shadow shadow = placeholder?.GetComponent<Shadow>();
+        if (shadow)
+            shadow.enabled = set;
+    }
+
+    private void ViewItem(InventoryItem item)
+    {
+        title.text = item.name;
+        description.text = item.description;
+        viewedItemImage.sprite = item.image;
+    }
+
+    private void SetupButtons()
+    {
+        int i = 0;
+        foreach (Button button in refs.buttons)
+        {
+            button.onClick.AddListener(() => SelectItem(i));
+            i++;
         }
     }
 
+    private List<Image> GetItemImages()
+    {
+        List<Image> images = new List<Image>(toolbar.GetComponentsInChildren<Image>());
+        images = images.FindAll(image => image.gameObject.name == "ItemImage");
+        return images;
+    }
+
+    private List<Image> GetItemPlaceholders()
+    {
+        List<Image> images = new List<Image>(toolbar.GetComponentsInChildren<Image>());
+        images = images.FindAll(image => image.gameObject.name == "Holder");
+        return images;
+    }
+
+    private List<Button> GetButtons()
+    {
+        List<Button> buttons = new List<Button>(toolbar.GetComponentsInChildren<Button>());
+        return buttons;
+    }
+
+    private int GetNumberOfItems()
+    {
+        return inventory.items.Count;
+    }
+
+    private InventoryItem GetItem(int index)
+    {
+        if (index >= GetNumberOfItems())
+        {
+            return null;
+        }
+
+        return inventory.items[index].item;
+    }
 }
